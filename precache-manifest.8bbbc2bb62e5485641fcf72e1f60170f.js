cc1c7522dec7c85404b96e869ea34a3a0d353bd4
24488 <jie.he01@hand-china.com>
2019-07-24 17:47:10 +0800

    Updates

---
+++ precache-manifest.8bbbc2bb62e5485641fcf72e1f60170f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b29c8c57d99cee46be85b549f6aa69",
+    "url": "/tolist/index.html"
+  },
+  {
+    "revision": "8da55f000b52a062c682",
+    "url": "/tolist/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cbf67aeb5d439c7b3abd",
+    "url": "/tolist/static/js/2.16024106.chunk.js"
+  },
+  {
+    "revision": "8da55f000b52a062c682",
+    "url": "/tolist/static/js/main.41a7762c.chunk.js"
+  },
+  {
+    "revision": "9fccadd0bc86083ad9b8",
+    "url": "/tolist/static/js/runtime~main.1f277893.js"
+  }
+]);
